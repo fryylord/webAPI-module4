a5782fb7746aa9b78c75db27174235a4d25a86e0
fryylord <rob@mchays.com>
2022-10-05 19:48:15 -0500

    timer created

---
+++ assets/script/script.js
@@ -0,0 +1,21 @@
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+
+function countdown() {
+    var timeLeft = 99;
+    var timeInterval = setInterval(function () {
+      if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+        timerEl.textContent = '';
+        clearInterval(timeInterval);
+        displayMessage();
+      }
+    }, 1000);
+  }
+
+  countdown();
