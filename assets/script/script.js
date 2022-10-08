d74e43937a0b33f0f17370d21b665d5a6862eb24
fryylord <rob@mchays.com>
2022-10-08 15:10:02 -0500

    revamped button method

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,5 @@
 var timerEl = document.getElementById('countdown');
+var testEl = document.getElementById('test')
 
 var timer =
 function countdown() {
@@ -17,49 +18,43 @@ function countdown() {
       }
     }, 1000);
   }
-
   
+  var buttonOne = document.createElement('button');
+  buttonOne.id = 'questOne';
+  buttonOne.className = 'questions'
+  buttonOne.innerHTML = 'Am I doing this right?';
+  testEl.appendChild(buttonOne);
   
-  // let questionOne ={
-  //   title: 'Best Quiz Ever',
-  //   alternatives: ['alpha', 'bravo', 'charlie', 'delta', 'echo', 'six'],
-  // }
-  const cont = document.getElementById('test');
 
-  const ul = document.createElement('ul');
-  ul.setAttribute ('style', 'padding: 0; margin: 0;');
-  ul.setAttribute('id', 'questOne');
+  var buttonTwo = document.createElement('button');
+  buttonTwo.id = 'questTwo';
+  buttonTwo.className = 'questions'
+  buttonTwo.innerHTML = 'I do not think so';
+  testEl.appendChild(buttonTwo);
   
-  var theList = ['Right', 'Wrong', 'Maybe Right', 'Maybe Wrong'];
-  var output = document.getElementById('test');
-  var getButtonList = function(items){
-      var html = '<ul>';
-      for(var item in items){
-          html += '<li><button>' + items[item] + '</button></li>';
-      }
-      html += '</ul>';
-      return html;
-      	
-  };
-  output.innerHTML = getButtonList( theList );
+  var buttonThree = document.createElement('button');
+  buttonThree.id = 'questThree';
+  buttonThree.className = 'questions'
+  buttonThree.innerHTML = 'What is wrong?';
+  testEl.appendChild(buttonThree);
 
-  // for (i = 0; i<= theList.length -1; i++) {
-  //   var x = document.querySelector('questOne');
-  //   x.addEventListener("click", alert('Button ' + i + ' was clicked!'))}
+  var buttonFour = document.createElement('button');
+  buttonFour.id = 'questFour';
+  buttonFour.className = 'questions'
+  buttonFour.innerHTML = 'Everything';
+  testEl.appendChild(buttonFour);
 
-  // for (i = 0; i <= questionOne.alternatives.length - 1; i++) {  
-  //   const li = document.createElement('button');
-  //     li.innerHTML = questionOne.alternatives[i];	                        
-  //     li.setAttribute ('style', 'display: block;');	
-  //     ul.appendChild(li);		
-  //     li.addEventListener('click', function() {
-        // alert('Button ' + i + ' was clicked!')})
-  //     cont.appendChild(ul);		
-  //   }
+  var correctAnswer = function corAns() {alert ("you got this right!")}
+  var wrongAnswer = function wrongAns() {alert ("you got this wrong!")}
+  
+  buttonOne.addEventListener("click", timer);
+  buttonOne.addEventListener("click", correctAnswer);
 
-  // for(var i = 0; i <= questOne.alternatives.length; i++){
-  //   var button = document.getElementById('button' + i);
-    
-
-  //   });
-  //   }
+  buttonTwo.addEventListener("click", timer);
+  buttonTwo.addEventListener("click", wrongAnswer);
+  
+  buttonThree.addEventListener("click", timer);
+  buttonThree.addEventListener("click", wrongAnswer);
+  
+  buttonFour.addEventListener("click", timer);
+  buttonFour.addEventListener("click", wrongAnswer);
